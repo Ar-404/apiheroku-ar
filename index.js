3933406765532d15043584e5e19647b5afbf4e51
Ar-404 <wolfweb19@gmail.com>
2020-09-23 21:48:17 +0530

    first commit

--- index.js
+++ index.js
@@ -12,6 +12,10 @@ app.get('/',(req,res)=>{
     res.send("Hello heroku apiheroku-ar");
 });
 
+app.get('/test',(req,res)=>{
+    res.send("Hello heroku test apiheroku-ar");
+});
+
 app.listen(port,()=>{
     console.log(`Server is running on http://localhost:${port}`);
 })
